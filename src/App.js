a12899486f5349c214d25fe3e774af164046be23
Tanner Collin <git@tannercollin.com>
2018-04-19 12:06:18 -0600

    Save generated sticker based on input

--- src/App.js
+++ src/App.js
@@ -1,56 +1,77 @@
 import React, { Component } from 'react';
 import domtoimage from 'dom-to-image';
-import { Form, Input } from 'semantic-ui-react'
+import FileSaver from 'file-saver';
+import { Button, Container, Form, Header, Icon, Input, Segment } from 'semantic-ui-react'
 import './App.css';
-
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 
 		this.sticker = React.createRef();
-		this.appBody = React.createRef();
 
 		this.state = {
-			messageText: 'What should I say?',
-			messageTime: '12:34 PM',
+			messageText: '',
+			messageTime: '',
 			canvas: []
 		};
+
+		this.messageChange = this.messageChange.bind(this);
+		this.timeChange = this.timeChange.bind(this);
+		this.saveSticker = this.saveSticker.bind(this);
 	}
 
-	componentDidMount() {
-		domtoimage.toPng(this.sticker.current).then(dataUrl => {
-			let img = new Image();
-			img.src = dataUrl;
-			this.appBody.current.appendChild(img);
+	messageChange(event) {
+		this.setState({messageText: event.target.value});
+	}
+
+	timeChange(event) {
+		this.setState({messageTime: event.target.value});
+	}
+
+	saveSticker() {
+		const fileName = this.state.messageText.replace(/\W/g, '').substring(0, 16) || 'sticker';
+		domtoimage.toBlob(this.sticker.current).then(blob => {
+			FileSaver.saveAs(blob, fileName + '.png');
 		});
 	}
 
 	render() {
 		return (
-			<div className='App' ref={this.appBody}>
-				<section className='chat__body'>
-					<div className='sticker' ref={this.sticker}>
-						<div className='chathead' />
-						<div className='messages'>
-
-							<div className='message droplet'>
-								<div className='message__text'>
-									<div className='message__text__content'>{this.state.messageText}
-										<div className='message__time'>{this.state.messageTime}</div>
+			<div>
+				<Segment inverted textAlign='center' style={{ marginBottom: '2em' }} vertical>
+					<Header as='h1' content='Omairchats' inverted style={{
+						fontSize: '3em',
+						fontWeight: 'normal',
+						marginBottom: '1em',
+						marginTop: '1em',
+					}}/>
+				</Segment>
+				<Container text>
+					<Form>
+						<Form.Group widths='equal'>
+							<Form.Field control={Input} label='Message' placeholder='What should I say?' onChange={this.messageChange}/>
+							<Form.Field control={Input} label='Time' placeholder='12:34 PM' onChange={this.timeChange}/>
+							<Form.Field control={Button} label='&nbsp;' color='grey' icon labelPosition='left' onClick={this.saveSticker}>
+								<Icon name='save' /> Save
+							</Form.Field>
+						</Form.Group>
+					</Form>
+					<section className='chat__body'>
+						<div className='sticker' ref={this.sticker}>
+							<div className='chathead' />
+							<div className='messages'>
+								<div className='message droplet'>
+									<div className='message__text'>
+										<div className='message__text__content'>{this.state.messageText || 'What should I say?'}
+											<div className='message__time'>{this.state.messageTime || '12:34 PM'}</div>
+										</div>
 									</div>
 								</div>
 							</div>
-
 						</div>
-					</div>
-				</section>
-			<Form>
-				<Form.Group widths='equal'>
-					<Form.Field control={Input} label='First name' placeholder='First name' />
-					<Form.Field control={Input} label='Last name' placeholder='Last name' />
-				</Form.Group>
-			</Form>
+					</section>
+				</Container>
 			</div>
 		);
 	}
